e70e0e7e2d53ac6c14ae6ba21cce758bc4fbf983
GSvenny <gabrielswagner7@gmail.com>
2022-07-07 00:44:19 -0500

    intern object test pass

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,6 +1,9 @@
-const { default: test } = require('node:test');
 const Intern = require('../lib/Intern');
 
 test("Creates a new intern object", () => {
-   
+    const intern = new Intern("Sarah", 222, "sarah@yahoo.com", "University of Minnesota");
+    expect(intern.name).toBe('Sarah');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe("sarah@yahoo.com");
+    expect(intern.school).toBe("University of Minnesota");
 })
