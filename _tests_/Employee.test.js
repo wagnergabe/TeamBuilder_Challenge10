a05573b78048969eef068b3cf48d73f2d679a383
GSvenny <gabrielswagner7@gmail.com>
2022-07-07 00:16:18 -0500

    fixed parent constructor (not all jobs require github)

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,10 +2,9 @@
 const Employee = require('../lib/Employee')
 
 test('Creates an employee Object', () => {
-    const employee = new Employee('Gabe', 123, "gabe@gmail.com", "wagnergabe");
+    const employee = new Employee('Gabe', 123, "gabe@gmail.com");
 
     expect(employee.name).toBe('Gabe');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe("gabe@gmail.com");
-    expect(employee.github).toBe('wagnergabe');
 });
