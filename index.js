c273b3512388d42dc06eeddc0aa1cff8db5433e0
GSvenny <gabrielswagner7@gmail.com>
2022-07-06 21:54:59 -0500

    prompt success

--- index.js
+++ index.js
@@ -6,3 +6,43 @@
 //Prompts divided for Manager | Engineer | Intern
 //Add Team member option after manager
 //generate HTML of selected team members.
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require ("util");
+// const Manager = require ("./lib/Manager");
+// const Engineer = require ("./lib/Engineer");
+// const Intern = require ("./lib/Intern");
+// const employee = require("./lib/Employee");
+
+TeamBuilder = () => {
+inquirer.prompt ([
+    {
+    type: 'input',
+    message: "name of employee",
+    name: "name"
+    },
+    {
+    type: 'input',
+    message: 'What is their eployee ID number?',
+    name: 'id'
+    },
+    {
+    type: 'input',
+    message: "What is their E-mail address?",
+    name: 'email'
+    },
+    {
+    type: 'input',
+    message: 'What is their github address?',
+    name: 'github'
+    },
+    {
+    type: 'input',
+    message: 'What is their office number?',
+    name: 'officeNumber'
+    }  
+  ]);
+}
+
+TeamBuilder();
