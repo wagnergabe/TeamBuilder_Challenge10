13f924a87e5a3399427df94b62a27e5614793588
GSvenny <gabrielswagner7@gmail.com>
2022-07-07 00:36:16 -0500

    engineer Object creater test pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,7 +1,13 @@
+const { exportAllDeclaration } = require('@babel/types');
 const Engineer = require('../lib/Engineer');
 // const Employee = require('../lib/Employee');
 
 test("Creates a New engineer object", () => {
-    
+    const engineer = new Engineer ('Bob', 111, "Bob@hotmail.com", "Engineer", "bobHub");
+    expect(engineer.name).toBe('Bob');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe("Bob@hotmail.com");
+    expect(engineer.role).toBe("Engineer");
+    expect(engineer.github).toBe("bobHub");
 })
 
