2fd030afcc8e284e238e3cac229d59e2ed2b5534
GSvenny <gabrielswagner7@gmail.com>
2022-07-07 00:06:08 -0500

    Employee object test pass

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,10 +2,10 @@
 const Employee = require('../lib/Employee')
 
 test('Creates an employee Object', () => {
-    const employee = new Employee('Gabe')
+    const employee = new Employee('Gabe', 123, "gabe@gmail.com", "wagnergabe");
 
     expect(employee.name).toBe('Gabe');
     expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe('string');
-    expect(employee.github).toBe('string');
+    expect(employee.email).toBe("gabe@gmail.com");
+    expect(employee.github).toBe('wagnergabe');
 });
