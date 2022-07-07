2423d39af1ad323a562110915d7e7bcd166d72ea
GSvenny <gabrielswagner7@gmail.com>
2022-07-06 21:35:01 -0500

    packages installed, pseudocode written

---
+++ index.js
@@ -0,0 +1,8 @@
+//TODO
+//Generate a webpage that displays team's basic info: emails/github profiles
+//generate a HTML file via command prompt
+//make sure email and github inputs have links to team member's accounts
+//Prompts for Team Member's: name, employee ID, email, and office number
+//Prompts divided for Manager | Engineer | Intern
+//Add Team member option after manager
+//generate HTML of selected team members.
